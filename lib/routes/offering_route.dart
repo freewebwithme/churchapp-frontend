4a42b2e24dd2cf730b09ebbcf6453dd6ee32c451
Taehwan Kim <freewebwithme777@protonmail.com>
2020-05-09 07:49:15 -0700

    Working on forms

--- lib/routes/offering_route.dart
+++ lib/routes/offering_route.dart
@@ -19,6 +19,8 @@ class _OfferingRouteState extends State<OfferingRoute> {
   String cvc;
   String amount;
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   void initState() {
     super.initState();
@@ -79,6 +81,7 @@ class _OfferingRouteState extends State<OfferingRoute> {
   }
 
   Widget build(BuildContext context) {
+    print("Prinitng width: ${MediaQuery.of(context).size.width}");
     return Scaffold(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
@@ -88,15 +91,30 @@ class _OfferingRouteState extends State<OfferingRoute> {
             textBottom:
                 "또 어떤 가난한 과부가 두 렙돈 넣는 것을 보시고이르시되 내가 참으로 너희에게 말하노니 이 가난한 과부가 다른 모든 사람보다 많이 넣었도다저들은 그 풍족한 중에서 헌금을 넣었거니와 이 과부는 그 가난한 중에서 자기가 가지고 있는 생활비 전부를 넣었느니라 하시니라 ",
           ),
-          Center(
-            child: SizedBox(
-              width: MediaQuery.of(context).size.width * 6,
-              child: RoundedButton(
-                  text: "연보하기",
-                  fontSize: 20,
-                  press: () {
-                    makeOffering();
-                  }),
+          Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("헌금하기"),
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "헌금 액수를 입력 하세요";
+                    }
+                    return null;
+                  },
+                ),
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * .6,
+                  child: RoundedButton(
+                      text: "연보하기",
+                      fontSize: 20,
+                      press: () {
+                        makeOffering();
+                      }),
+                ),
+              ],
             ),
           ),
         ],
